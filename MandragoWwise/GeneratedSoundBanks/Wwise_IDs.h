a62beb5ee76cf8b0ac3a8e99c460292135ad82c5
Youé Graillot <youe.graillot@gmail.com>
2018-12-13 11:44:55 +0100

    Up prefs

---
+++ MandragoWwise/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,91 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID DRAW_PLAY = 2083537832U;
+        static const AkUniqueID DRAW_STOP = 166240794U;
+        static const AkUniqueID PLANET_ORBIT_PLAY = 3908603903U;
+        static const AkUniqueID PLANET_ORBIT_STOP = 3441388573U;
+        static const AkUniqueID PORTAL_PLAY = 1394292256U;
+        static const AkUniqueID PORTAL_STOP = 4032518562U;
+        static const AkUniqueID SUN_PLAY = 3578106302U;
+        static const AkUniqueID SUN_STOP = 279455104U;
+        static const AkUniqueID TELEKINESIS_GRAB_PLAY = 350557399U;
+        static const AkUniqueID TELEKINESIS_HAND_PLAY = 3425656180U;
+        static const AkUniqueID TELEKINESIS_HAND_STOP = 834591310U;
+        static const AkUniqueID TELEKINESIS_OBJECT_PLAY = 2323001974U;
+        static const AkUniqueID TELEKINESIS_OBJECT_STOP = 3095115784U;
+        static const AkUniqueID TELEPORTATION_PLAY = 567745568U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace PLANETS_ORBIT
+        {
+            static const AkUniqueID GROUP = 2061498879U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FOUR = 2863728729U;
+                static const AkUniqueID ONE = 1064933119U;
+                static const AkUniqueID THREE = 912956111U;
+                static const AkUniqueID TWO = 678209053U;
+            } // namespace STATE
+        } // namespace PLANETS_ORBIT
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DRAW = 1912241455U;
+        static const AkUniqueID PLAYBACK_RATE = 1524500807U;
+        static const AkUniqueID RPM = 796049864U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+        static const AkUniqueID TELEKINESIS_SPEED = 2497914097U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK = 1661994096U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBISONIC = 1034652824U;
+        static const AkUniqueID AURO_3D = 2856286951U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MOTION_FACTORY_BUS = 985987111U;
+        static const AkUniqueID SURROUND = 2967106803U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID DEFAULT_MOTION_DEVICE = 4230635974U;
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
